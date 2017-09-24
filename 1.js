b4a30f823f35cbaaafb80a8cf5ffa1cb9fc85088
Nazar82 <n.viazivsky@gmail.com>
2017-09-24 14:15:19 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,11 +2,7 @@
  //This code does not work for all test cases. I think it's because of caution in the instructions to this kata
  
 function newsIsFalse(news) {
-
-const fixedNews = news.replace(/[^abigl]/g, ``);
-
-let re = /\w*a\w*b\w*i\w*g\w*a\w*i\w*l\w*/;
-  
-return re.test(fixedNews) ? `YES`: `NO`;
-      
+  const fixedNews = news.replace(/[^abigl]/g, ``);
+  let re = /\w*a\w*b\w*i\w*g\w*a\w*i\w*l\w*/;
+  return re.test(fixedNews) ? `YES`: `NO`;
 }
