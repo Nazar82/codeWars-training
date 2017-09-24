08a42c73678d9d5e97f3d1702c6b92b88b9e19ce
Nazar82 <n.viazivsky@gmail.com>
2017-09-24 09:48:32 +0000

    1 kata

---
+++ 1.js
@@ -0,0 +1,10 @@
+ //http://www.codewars.com/kata/the-fools-day-1/train/javascript
+ //This code does not work for all test cases. I think it's because of caution in the instructions to this kata
+ 
+function newsIsFalse(news) {
+
+let re = /\w*a\w*b\w*i\w*g\w*a\w*i\w*l\w*/;
+  
+return re.test(news) ? `YES`: `NO`;
+   
+}
