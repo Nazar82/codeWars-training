b63d3c58dfb9ab63ec72d9c474174205a2937754
Nazar82 <n.viazivsky@gmail.com>
2017-09-24 16:25:27 +0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,7 +1,7 @@
  //http://www.codewars.com/kata/consecutive-count/train/javascript
  
 function getConsectiveItems(items, key){
-  let re = new RegExp(`${key}+`, `g`);
-  let matchedItems = items.toString().match(re);
-  return matchedItems === null ? 0 : matchedItems.sort((a, b) => b.length - a.length)[0].length;
+  const re = new RegExp(`${key}+`, 'g');
+  const matchedItems = String(items).match(re);
+  return matchedItems ? matchedItems.reduce( (a, b) => a > b ? a : b ).length : 0;
 }
